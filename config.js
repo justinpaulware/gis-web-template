771c5668c0d90005d7af555107efd461304db087
justinpaulware <77998303+justinpaulware@users.noreply.github.com>
2026-06-16 20:11:35 -0400

    config.js

--- config.js
+++ config.js
@@ -1,37 +1,5 @@
 const CONFIG = {
-
-  filters: [
-    {
-      id: "category",
-      label: "Category",
-      field: "Category",
-      options: ["All", "Community Asset", "Opportunity Site", "Concern", "Vacant Building"]
-    }
-  ],
-
-  layers: [
-    {
-      id: "community",
-      name: "Community Points",
-      type: "sheet",
-      url: "PASTE_GOOGLE_SHEET_CSV_URL",
-      group: "Community",
-
-      style: "circle",
-
-      colorField: "Category",
-      colorMap: {
-        "Community Asset": "#2563eb",
-        "Opportunity Site": "#f59e0b",
-        "Concern": "#ef4444",
-        "Vacant Building": "#6b7280"
-      },
-
-      paint: {
-        "circle-radius": 6,
-        "circle-stroke-width": 1,
-        "circle-stroke-color": "#ffffff"
-      }
-    }
-  ]
+    center: [-73.9209, 41.2901],
+    zoom: 8,
+    basemap: "https://tiles.openfreemap.org/styles/positron"
 };
